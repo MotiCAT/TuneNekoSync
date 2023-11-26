28213dce8350af8645b5473783da45ac0b7e1835
MotiCAT <youyuzhangye@gmail.com>
2023-11-27 00:43:58 +0900

    chore:remove exts

--- .prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    printWidth: 150, // 自動折返し文字数
-    trailingComma: 'es5', // 複数行の場合、可能な限り末尾のコンマを出力
-    tabWidth: 4, // タブサイズ
-    semi: true, // ステートメントの最後にセミコロンを付与
-    singleQuote: true, // 二重引用符の代わりに単一引用符を使用
-    endOfLine: 'auto', // 改行の文字コードを指定
-  };
