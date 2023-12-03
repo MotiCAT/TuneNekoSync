34c586c113191b5a2281ffbc79db763fa20fb459
MotiCAT <youyuzhangye@gmail.com>
2023-12-03 17:50:57 +0900

    ⚡️Send message at stop

--- src/commands/stop.ts
+++ src/commands/stop.ts
@@ -1,9 +1,12 @@
 import { queueManager } from '../classes/queue';
-import { Message } from 'discord.js';
+import { Message, EmbedBuilder } from 'discord.js';
 
 export async function stopCommand(message: Message): Promise<void> {
 	if (message.guild?.members.me?.voice.channel) {
 		message.guild.members.me.voice.disconnect();
+		message.reply({
+			embeds: [new EmbedBuilder().addFields({ name: 'Success', value: '再生を停止しました。' }).setColor('Green')]
+		});
 	}
 
 	queueManager.queues.delete(message.guildId!);
