ea7f5682d26b61a9bd1deaa921e6070f3a1bdf52
ysr9029 <56439343+Nich87@users.noreply.github.com>
2023-12-03 13:51:00 +0900

    🐛 added command alias

--- src/Events/onMessageCreate.ts
+++ src/Events/onMessageCreate.ts
@@ -10,33 +10,48 @@ export async function onMessageCreate(message: Message): Promise<Awaitable<void>
 
 	switch (commandName) {
 		case 'play':
+		case 'p':
 			commands.play(message);
 			break;
 		case 'stop':
+		case 'leave':
 			commands.stop(message);
 			break;
 		case 'pause':
 			commands.pause(message);
 			break;
 		case 'resume':
+		case 'continue':
+		case 're':
 			commands.resume(message);
 			break;
 		case 'loop':
+		case 'repeat':
+		case 'l':
 			commands.loop(message, args);
 			break;
 		case 'skip':
+		case 's':
+		case 'next':
 			commands.skip(message);
 			break;
 		case 'queue':
+		case 'q':
+		case 'list':
 			commands.queue(message);
 			break;
 		case 'help':
+		case 'h':
 			commands.help(message);
 			break;
 		case 'volume':
+		case 'vol':
+		case 'v':
 			commands.changeVolume(message);
 			break;
 		case 'nowplaying':
+		case 'np':
+		case 'current':
 			commands.nowplaying(message);
 			break;
 		default:
