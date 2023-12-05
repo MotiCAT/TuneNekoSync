bd42b3f2d8fc735480100434697a07fa79935e58
MotiCAT <youyuzhangye@gmail.com>
2023-12-05 23:15:03 +0900

    🩹Change of nowplaying reply method

--- src/interactions/nowplaying.ts
+++ src/interactions/nowplaying.ts
@@ -5,9 +5,10 @@ import { client } from '../index';
 import { ChatInputCommandInteraction } from 'discord.js';
 
 export async function nowplayingCommand(interaction: ChatInputCommandInteraction) {
+	await interaction.deferReply();
 	const player = client?.player;
-	if (typeof player === 'undefined') return interaction.reply(embeds.videoNotPlaying);
+	if (typeof player === 'undefined') return interaction.followUp(embeds.videoNotPlaying);
 	const queue = queueManager.getQueue(interaction.guild?.id as string) as Queue;
 	const info = await getSongInfo(queue.currentSong);
-	return interaction.reply(info);
+	return interaction.followUp(info);
 }
