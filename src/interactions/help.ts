d782957f3f2f41e101de75e84194bdcaef70826a
MotiCAT <youyuzhangye@gmail.com>
2023-12-16 13:41:06 +0900

    🩹Slash command support for help menu

--- src/interactions/help.ts
+++ src/interactions/help.ts
@@ -1,6 +1,28 @@
-import { embeds } from '../embeds';
-import { ChatInputCommandInteraction } from 'discord.js';
+import {
+	ActionRowBuilder,
+	ChatInputCommandInteraction,
+	StringSelectMenuBuilder,
+	StringSelectMenuOptionBuilder
+} from 'discord.js';
 
 export async function helpCommand(interaction: ChatInputCommandInteraction) {
-	interaction.reply(embeds.help);
+	const menu = new StringSelectMenuBuilder()
+		.setCustomId('help')
+		.setPlaceholder('Select a category')
+		.addOptions([
+			new StringSelectMenuOptionBuilder()
+				.setLabel('再生操作')
+				.setValue('music')
+				.setDescription('Commands related to music'),
+			new StringSelectMenuOptionBuilder()
+				.setLabel('情報取得')
+				.setValue('misc')
+				.setDescription('Commands related to getting information'),
+			new StringSelectMenuOptionBuilder()
+				.setLabel('検索')
+				.setValue('search')
+				.setDescription('Commands related to searching')
+		]);
+	const row = new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(menu);
+	return interaction.reply({ content: 'カテゴリーを選択してください！', components: [row] });
 }
